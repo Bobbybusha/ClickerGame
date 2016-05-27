358f003fe5510d452feaecbde3f1a862001fb61b
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-05-27 20:56:46 +0200

    Update 1.0.1 - libGdx project files
    generate by gdx-setup for IDEA IDE

---
+++ desktop/src/pl/newstech/clickergame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.newstech.clickergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.newstech.clickergame.ClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClickerGame(), config);
+	}
+}
