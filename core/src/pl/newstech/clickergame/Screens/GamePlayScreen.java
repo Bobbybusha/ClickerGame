cd2259209aa0d998267015f1dc524150a8a60e32
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-05-28 14:25:40 +0200

    Update 1.0.7 - GamePlayScreen class

---
+++ core/src/pl/newstech/clickergame/Screens/GamePlayScreen.java
@@ -0,0 +1,17 @@
+package pl.newstech.clickergame.Screens;
+
+import pl.newstech.clickergame.ClickerGame;
+
+/**
+ * Created by bartek on 28.05.16.
+ */
+public class GamePlayScreen extends AbstractScreen {
+    public GamePlayScreen(ClickerGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+        
+    }
+}
